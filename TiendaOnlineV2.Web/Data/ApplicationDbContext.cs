9be9b8c9ef86c95b386250c28ce28e494eb61978
JHON ALEJANDRO GARCIA ECHAVARRIA <jhongarcia288842@correo.itm.edu.co>
2022-04-22 23:51:40 -0500

    twostep

---
+++ TiendaOnlineV2.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaOnlineV2.Web.Models;
+
+namespace TiendaOnlineV2.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        
+        public DbSet<Country> Countries { get; set; }
+        //public DbSet<Department> Departments { get; set; }
+        //public DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+            //modelBuilder.Entity<Department>().HasIndex("Name", "CountryId").IsUnique();
+            //modelBuilder.Entity<City>().HasIndex("Name", "DepartmentId").IsUnique();
+        }
+    }
+}
---
+++ TiendaOnlineV2.Web/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaOnlineV2.Web.Models
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe contener al menos un caracter")]
+        public string Name { get; set; }
+    }
+}
